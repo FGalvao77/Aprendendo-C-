3fbb9fdb9a0a74ae6ab9e36a2d75440e883a3949
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2022-02-18 13:18:19 -0300

    Add files via upload

---
+++ 01_MyFirstCode.cpp
@@ -0,0 +1,13 @@
+//My first code in language C++
+//Printing "Hello, world!" in the screen
+
+
+#include <iostream>
+#include <stdlib.h>
+
+int main(){
+    std::cout << "Hello, world!\n";
+
+    system("pause");
+    return 0;
+}
